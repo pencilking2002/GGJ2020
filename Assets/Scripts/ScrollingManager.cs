d619e141922efe3221c8cafbb58412a64bbd9409
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-02-01 14:06:12 -0500

    Scroll system updates

--- Assets/Scripts/ScrollingManager.cs
+++ Assets/Scripts/ScrollingManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class ScrollingManager : MonoBehaviour
 {
-    public int numModules = 3;
+    public GameObject[] modules;
+    public float scrollSpeed = 5.0f;
+    public Camera cam;
+
 
     private void Update()
     {
-        
+        var cutoffPoint = cam.ScreenToWorldPoint(new Vector3(0.5f, cam.pixelHeight, 10));
+        for(int i=0; i<modules.Length; i++)
+        {
+            //if (modules[i].transform.)
+            modules[i].transform.localPosition += new Vector3(0,scrollSpeed * Time.deltaTime,0);
+        }
     }
 }
