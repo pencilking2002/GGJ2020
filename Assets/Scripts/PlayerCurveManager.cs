22bad52c931db0e17b61c69e7bf80b3e5691903e
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-02-01 18:53:18 -0500

    Curve point detection updates

--- Assets/Scripts/PlayerCurveManager.cs
+++ Assets/Scripts/PlayerCurveManager.cs
@@ -8,10 +8,15 @@ public class PlayerCurveManager : MonoBehaviour
 {
     public BGCurve curve;
     public BGCcMath curveMath;
+    public PlayerController player;
+    public float threshold = 5.0f;
+    public bool isNearCurve;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
+        Vector3 closestPoint = Vector3.zero;
+        closestPoint = curveMath.Math.CalcPositionByClosestPoint(player.transform.position);
         for(int sIndex=0; sIndex<curveMath.Math.SectionsCount; sIndex++)
         {
             for(int i=0; i<curveMath.Math.SectionInfos[sIndex].PointsCount; i++)
@@ -19,5 +24,18 @@ public class PlayerCurveManager : MonoBehaviour
                 Gizmos.DrawSphere(curveMath.Math.SectionInfos[sIndex].Points[i].Position, 1);
             }
         }
+
+        if (isNearCurve)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(closestPoint, 1.1f);
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 closestPoint = Vector3.zero;
+        closestPoint = curveMath.Math.CalcPositionByClosestPoint(player.transform.position);
+        isNearCurve = (closestPoint != Vector3.zero && Vector2.Distance(closestPoint, player.transform.position) < threshold);
     }
 }
