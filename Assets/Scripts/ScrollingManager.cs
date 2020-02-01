902803a7ef4c8a5cc32e2e3152b39a9288c757a1
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-02-01 13:41:57 -0500

    Started work on the scrolling system

---
+++ Assets/Scripts/ScrollingManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingManager : MonoBehaviour
+{
+    public int numModules = 3;
+
+    private void Update()
+    {
+        
+    }
+}
