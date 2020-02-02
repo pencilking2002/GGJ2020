5f2d2d57240e38a355bb23c2e3bb93a461248961
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-02-02 09:20:48 -0500

    Player has ability to draw a line

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using System;
 
 public class PlayerController : MonoBehaviour
 {
+    public LineDrawing lineDrawer;
     public PlayerModuleCollider moduleCollider;
     [SerializeField] private Transform leftLimit;
     [SerializeField] private Transform rightLimit;
