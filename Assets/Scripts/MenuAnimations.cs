32ee8bf1cbc35c0c25081c11e53eae6314a08d7a
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-02-02 23:15:11 -0500

    Menu refinements

--- Assets/Scripts/MenuAnimations.cs
+++ Assets/Scripts/MenuAnimations.cs
@@ -8,7 +8,7 @@ public class MenuAnimations : MonoBehaviour
     [SerializeField] CanvasGroup startButton;
     [SerializeField] float animDelay = 1.0f;
     [SerializeField] float animTime = 1.0f;
-
+    [SerializeField] float logoMoveDistance = 20.0f;
     private void Awake()
     {
         InitMenuAnimSequence();
@@ -22,12 +22,12 @@ public class MenuAnimations : MonoBehaviour
 
             // Fade in and move the logo up
             var targetPos = ruptureLogo.transform.position;
-            ruptureLogo.transform.position -= Vector3.up * 20.0f;
+            ruptureLogo.transform.position -= Vector3.up * logoMoveDistance;
 
-            LeanTween.move(ruptureLogo.gameObject, targetPos, animTime).setEaseOutExpo();
-            LeanTween.alphaCanvas(ruptureLogo, 1.0f, animTime).setEaseInOutExpo().setOnComplete(() => {
+            LeanTween.alphaCanvas(ruptureLogo, 1.0f, animTime * 0.6f).setEaseOutCubic();
+            LeanTween.move(ruptureLogo.gameObject, targetPos, animTime).setEaseOutCubic().setOnComplete(() => {
                 // Fade in the start button
-                LeanTween.alphaCanvas(startButton, 1.0f, animTime * 0.5f);
+                LeanTween.alphaCanvas(startButton, 1.0f, animTime * 0.2f);
             });
 
 
