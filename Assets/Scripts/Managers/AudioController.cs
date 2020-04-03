5b0cf7c51c769bf6f92072018a6109fb58344b20
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-04-03 15:38:25 -0400

    - Added menu and game music - Created an AudioController

---
+++ Assets/Scripts/Managers/AudioController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioClip menuMusicClip;
+    [SerializeField] private AudioClip gameMusicClip;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = menuMusicClip;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}   
