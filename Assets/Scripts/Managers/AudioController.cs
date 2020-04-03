9f10db662471c5971be8c1562ba55c12a25dfcef
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-04-03 17:05:18 -0400

    Implemented menu and game music

--- Assets/Scripts/Managers/AudioController.cs
+++ Assets/Scripts/Managers/AudioController.cs
@@ -7,13 +7,36 @@ public class AudioController : MonoBehaviour
     [SerializeField] private AudioClip menuMusicClip;
     [SerializeField] private AudioClip gameMusicClip;
 
-    private AudioSource audioSource;
+    [SerializeField] private AudioSource menuAudioSource;
+    [SerializeField] private AudioSource gameAudioSource;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = menuMusicClip;
-        audioSource.loop = true;
-        audioSource.Play();
+        menuAudioSource.Play();
+    }
+
+    private void GameStarted()
+    {
+        var seq = LeanTween.sequence();
+        seq.append(LeanTween.value(1.0f, 0.0f, 3.0f).setOnUpdate(val => {
+            menuAudioSource.volume = val;
+        }));        
+
+        seq.append(() => {
+            menuAudioSource.Stop();
+        });
+
+        gameAudioSource.Play();
+
+    }
+
+    private void OnEnable()
+    {
+        MenuManager.OnGameStart += GameStarted;
+    }
+
+    private void OnDisable()
+    {
+        MenuManager.OnGameStart -= GameStarted;
     }
 }   
