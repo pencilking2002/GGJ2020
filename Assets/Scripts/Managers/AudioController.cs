8e2243afe2bd9cb22a05b1d419f98d15b18bc586
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-04-03 17:13:27 -0400

    game over audio fix

--- Assets/Scripts/Managers/AudioController.cs
+++ Assets/Scripts/Managers/AudioController.cs
@@ -25,18 +25,35 @@ public class AudioController : MonoBehaviour
         seq.append(() => {
             menuAudioSource.Stop();
         });
-
+        gameAudioSource.volume = 1.0f;
         gameAudioSource.Play();
 
+    }
+
+    private void GameOver()
+    {
+        var seq = LeanTween.sequence();
+        seq.append(LeanTween.value(1.0f, 0.0f, 1.0f).setOnUpdate(val => {
+            gameAudioSource.volume = val;
+        }));        
+
+        seq.append(() => {
+            gameAudioSource.Stop();
+        });
+        menuAudioSource.volume = 1.0f;
+        menuAudioSource.Play();
     }
 
     private void OnEnable()
     {
         MenuManager.OnGameStart += GameStarted;
+        MenuManager.OnGameOver += GameOver;
     }
+
 
     private void OnDisable()
     {
         MenuManager.OnGameStart -= GameStarted;
+        MenuManager.OnGameOver -= GameOver;
     }
 }   
