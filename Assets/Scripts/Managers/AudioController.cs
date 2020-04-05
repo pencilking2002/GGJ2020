3d0d9f9f513b219f73aaa8745f345a13ea65913d
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-04-04 22:48:22 -0400

    Added drill sound and logic

--- Assets/Scripts/Managers/AudioController.cs
+++ Assets/Scripts/Managers/AudioController.cs
@@ -4,15 +4,107 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+    [Header("Audio Clips")]
     [SerializeField] private AudioClip menuMusicClip;
     [SerializeField] private AudioClip gameMusicClip;
+    [SerializeField] private AudioClip goodDrillClip;
+    [SerializeField] private AudioClip badDrillClip;
+    [SerializeField] private AudioClip badEmptyDrillClip;
 
+    [Header("Audio Sources")]
     [SerializeField] private AudioSource menuAudioSource;
     [SerializeField] private AudioSource gameAudioSource;
+
+    [SerializeField] private AudioSource drillAudioSource;
+
+    private PlayerController player;
 
     private void Start()
     {
         menuAudioSource.Play();
+        player = GameManager.Instance.player;
+    }
+
+    private void Update()
+    {
+        if (player.IsPlayerWelding())
+        {
+            if (player.isWeldingOnCurve)
+            {
+                if (player.moduleCollider.isColliding)
+                {
+                    if (!drillAudioSource.isPlaying || drillAudioSource.clip != goodDrillClip)
+                    {
+                        drillAudioSource.clip = goodDrillClip;
+                        drillAudioSource.Play();
+                    }
+                }
+                else
+                {
+                    if (!drillAudioSource.isPlaying || drillAudioSource.clip != badEmptyDrillClip)
+                    {
+                        drillAudioSource.clip = badEmptyDrillClip;
+                        drillAudioSource.Play();
+                    }
+                }
+            }
+            else
+            {
+                if (player.moduleCollider.isColliding)
+                {
+                    if (!drillAudioSource.isPlaying || drillAudioSource.clip != badDrillClip)
+                    {
+                        drillAudioSource.clip = badDrillClip;
+                        drillAudioSource.Play();
+                    }
+                }
+                else
+                {
+                    if (!drillAudioSource.isPlaying || drillAudioSource.clip != badEmptyDrillClip)
+                    {
+                        drillAudioSource.clip = badEmptyDrillClip;
+                        drillAudioSource.Play();
+                    }
+                }
+            }
+        }
+        else if (drillAudioSource.isPlaying)
+        {
+            drillAudioSource.Stop();
+        }
+        
+        // if (GameManager.Instance.player.IsPlayerWelding() && !GameManager.Instance.player.moduleCollider.isColliding)
+        // {
+        //     if (!drillAudioSource.isPlaying || drillAudioSource.clip != badEmptyDrillClip)
+        //     {
+        //         drillAudioSource.Stop();
+        //         drillAudioSource.clip = badEmptyDrillClip;
+        //         drillAudioSource.Play();
+        //     }
+        // }
+        // else if (GameManager.Instance.player.isWeldingOnCurve)
+        // {
+        //     if (!drillAudioSource.isPlaying || drillAudioSource.clip == badDrillClip)
+        //     {
+        //         drillAudioSource.Stop();
+        //         drillAudioSource.clip = goodDrillClip;
+        //         drillAudioSource.Play();
+        //     }
+        // }
+        // else if (GameManager.Instance.player.IsPlayerWelding())
+        // {
+        //     if (!drillAudioSource.isPlaying || drillAudioSource.clip == goodDrillClip)
+        //     {
+        //         drillAudioSource.Stop();
+        //         drillAudioSource.clip = badDrillClip;
+        //         drillAudioSource.Play();
+        //     }
+            
+        // }
+        // else if (drillAudioSource.isPlaying)
+        // {
+        //     drillAudioSource.Stop();
+        // }
     }
 
     private void GameStarted()
